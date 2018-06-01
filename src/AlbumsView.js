8fc9276584499cc5ca8e3eb3487b7cbaa3166786
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-06-01 10:42:26 +0200

    index.html: Changed title of the app, Added CDN link to bootstrap; AlbumsView: Added bootstrap initial layout

--- src/AlbumsView.js
+++ src/AlbumsView.js
@@ -1,14 +1,23 @@
 import React, {Component, Fragment} from 'react'
+import {Grid, Row, Col} from 'react-bootstrap'
 
 class AlbumsView extends Component {
   render() {
     return (
-      <Fragment
-        className="PhotoAlbums"
-      >
-        <div className="PhotoAlbums__Album">Album 1</div>
-        <div className="PhotoAlbums__Album">Album 2</div>
-        <div className="PhotoAlbums__Album">Album 3</div>
+      <Fragment>
+        <Grid>
+          <Row>
+            <Col md={4}>
+              <div className="PhotoAlbums__Album">Album 1</div>
+            </Col>
+            <Col md={4}>
+              <div className="PhotoAlbums__Album">Album 2</div>
+            </Col>
+            <Col md={4}>
+              <div className="PhotoAlbums__Album">Album 3</div>
+            </Col>
+          </Row>
+        </Grid>
       </Fragment>
     )
   }
