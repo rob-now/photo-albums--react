b6e97199ca9401011aee1e7675053ff817a36388
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-06-01 12:17:27 +0200

    Added PhotosGridView component and connecting it to Router

---
+++ src/PhotosGridView.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class PhotosGridView extends Component {
+  render() {
+    return (
+      <h2>Photos grid view</h2>
+    )
+  }
+}
+
+export default PhotosGridView
--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,15 @@
 import React, {Component, Fragment} from 'react'
 import {Route} from 'react-router-dom'
+
 import AlbumsView from './AlbumsView'
+import PhotosGridView from "./PhotosGridView";
 
 class Routes extends Component {
   render() {
     return (
       <Fragment>
         <Route exact path="/" component={AlbumsView}/>
+        <Route path="/photos-grid" component={PhotosGridView}/>
       </Fragment>
     )
   }
