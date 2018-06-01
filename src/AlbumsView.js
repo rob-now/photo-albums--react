de5d255bdad69315e195602879974beb919ce48b
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-06-01 10:22:47 +0200

    Installed react-bootstrap and initial AlbumsView content

--- src/AlbumsView.js
+++ src/AlbumsView.js
@@ -1,9 +1,15 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
 
 class AlbumsView extends Component {
   render() {
     return (
-      <div></div>
+      <Fragment
+        className="PhotoAlbums"
+      >
+        <div className="PhotoAlbums__Album">Album 1</div>
+        <div className="PhotoAlbums__Album">Album 2</div>
+        <div className="PhotoAlbums__Album">Album 3</div>
+      </Fragment>
     )
   }
 }
