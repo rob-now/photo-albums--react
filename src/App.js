27856cbe0a33ae47fdbf29ed2891a5f187ef051b
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-06-01 10:10:08 +0200

    Created AlbumsView

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import AlbumsView from "./AlbumsView";
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <AlbumsView/>
       </div>
     );
   }
