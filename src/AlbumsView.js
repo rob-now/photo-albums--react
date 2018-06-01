b0581fe9056946e01ac78be828111262f948834a
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-06-01 11:23:08 +0200

    Initial setup of placeholder images in AlbumsView

--- src/AlbumsView.js
+++ src/AlbumsView.js
@@ -1,5 +1,6 @@
 import React, {Component, Fragment} from 'react'
 import {Grid, Row, Col} from 'react-bootstrap'
+import './AlbumsView.css'
 
 class AlbumsView extends Component {
   render() {
@@ -8,13 +9,16 @@ class AlbumsView extends Component {
         <Grid>
           <Row>
             <Col md={4}>
-              <div className="PhotoAlbums__Album">Album 1</div>
+              <img src="http://via.placeholder.com/800x600" className="img-responsive PhotoAlbums__Album" alt="Album 1"/>
             </Col>
             <Col md={4}>
-              <div className="PhotoAlbums__Album">Album 2</div>
+              <img src="http://via.placeholder.com/800x600" className="img-responsive PhotoAlbums__Album" alt="Album 2"/>
             </Col>
             <Col md={4}>
-              <div className="PhotoAlbums__Album">Album 3</div>
+              <img src="http://via.placeholder.com/800x600" className="img-responsive PhotoAlbums__Album" alt="Album 3"/>
+            </Col>
+            <Col md={4}>
+              <img src="http://via.placeholder.com/800x600" className="img-responsive PhotoAlbums__Album" alt="Album 3"/>
             </Col>
           </Row>
         </Grid>
