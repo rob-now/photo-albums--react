94b81366080a45c4addcef433639b369e700a616
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-06-01 11:40:43 +0200

    Mapping through albums and displaying them

--- src/AlbumsView.js
+++ src/AlbumsView.js
@@ -2,24 +2,39 @@ import React, {Component, Fragment} from 'react'
 import {Grid, Row, Col} from 'react-bootstrap'
 import './AlbumsView.css'
 
+const albums = [
+  {
+    id: 0,
+    url: 'http://via.placeholder.com/800x600'
+  },
+  {
+    id: 1,
+    url: 'http://via.placeholder.com/800x600'
+  },
+  {
+    id: 2,
+    url: 'http://via.placeholder.com/800x600'
+  },
+  {
+    id: 3,
+    url: 'http://via.placeholder.com/800x600'
+  }
+]
+
 class AlbumsView extends Component {
   render() {
     return (
       <Fragment>
         <Grid>
           <Row>
-            <Col md={4}>
-              <img src="http://via.placeholder.com/800x600" className="img-responsive PhotoAlbums__Album" alt="Album 1"/>
-            </Col>
-            <Col md={4}>
-              <img src="http://via.placeholder.com/800x600" className="img-responsive PhotoAlbums__Album" alt="Album 2"/>
-            </Col>
-            <Col md={4}>
-              <img src="http://via.placeholder.com/800x600" className="img-responsive PhotoAlbums__Album" alt="Album 3"/>
-            </Col>
-            <Col md={4}>
-              <img src="http://via.placeholder.com/800x600" className="img-responsive PhotoAlbums__Album" alt="Album 3"/>
-            </Col>
+            {
+              albums && albums.map(
+                ({id, url}) =>
+                  <Col key={id} md={4}>
+                    <img src={url} className="img-responsive PhotoAlbums__Album" alt={`Album ${id + 1}`}/>
+                  </Col>
+              )
+            }
           </Row>
         </Grid>
       </Fragment>
