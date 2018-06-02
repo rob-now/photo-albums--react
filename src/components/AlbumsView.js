94d6978c1d883971c697c388af284aec9b575e1a
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-06-02 23:55:35 +0200

    Corrected hover effect issue on AlbumsView

--- src/components/AlbumsView.js
+++ src/components/AlbumsView.js
@@ -20,22 +20,25 @@ class AlbumsView extends Component {
                   <Col
                     key={id}
                     md={4}
+                    className="bs--col"
                   >
-                    <img
-                      src={url}
-                      className={`img-responsive PhotoAlbums__Album--${id}`}
-                      alt={`Album ${id + 1}`}
-                    />
-                    <div
-                      className="PhotoAlbums__Album--Description--Container"
-                      onClick={() =>
-                        this.props.history.push(`/album/${id}`)}
-                    >
-                      <p
-                        className="PhotoAlbums__Album--Description"
+                    <div className="PhotoAlbums__Album--Container">
+                      <img
+                        src={url}
+                        className={`img-responsive PhotoAlbums__Album--${id}`}
+                        alt={`Album ${id + 1}`}
+                      />
+                      <div
+                        className="PhotoAlbums__Album--Description--Container"
+                        onClick={() =>
+                          this.props.history.push(`/album/${id}`)}
                       >
-                        {albumName || `Photo album ${id + 1}`}
-                      </p>
+                        <p
+                          className="PhotoAlbums__Album--Description"
+                        >
+                          {albumName || `Photo album ${id + 1}`}
+                        </p>
+                      </div>
                     </div>
                   </Col>
               )
