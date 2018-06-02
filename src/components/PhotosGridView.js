0091e330b35290941da10ca1d16ee85e7e52c4ec
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-06-02 12:52:45 +0200

    Added arrow left for going back to albums view

--- src/components/PhotosGridView.js
+++ src/components/PhotosGridView.js
@@ -1,6 +1,7 @@
 import React, {Component, Fragment} from 'react'
 import PhotoItem from "./PhotoItem";
 import './PhotosGridView.css'
+import 'font-awesome/css/font-awesome.min.css'
 import {withAlbums} from "../contexts/Albums";
 
 class PhotosGridView extends Component {
@@ -11,12 +12,20 @@ class PhotosGridView extends Component {
   render() {
     return (
       <Fragment>
-        <button
-          onClick={this.handleGoBackToAlbums}
-        >
-          Go back to Albums
-        </button>
-        <h1>Photos from <span className="PhotosGridView__AlbumNumber">album {parseInt(this.props.match.params.albumId, 10) + 1}</span></h1>
+        {/*<button*/}
+          {/*onClick={this.handleGoBackToAlbums}*/}
+        {/*>*/}
+          {/*Go back to Albums*/}
+        {/*</button>*/}
+        <h1>
+          <i
+            className="fas fa-arrow-circle-left"
+            onClick={this.handleGoBackToAlbums}
+          >&nbsp;</i>
+          Photos from <span
+          className="PhotosGridView__AlbumNumber">album {parseInt(this.props.match.params.albumId, 10) + 1}
+          </span>
+        </h1>
         <PhotoItem {...this.props}/>
       </Fragment>
     )
